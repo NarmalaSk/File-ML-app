2efdab4f52f3fc32f71a62859308e3f608596279
NarmalaSk <shashinarmala29@gmail.com>
2024-10-19 23:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "bumblebee"
-
+@app.route('/login')
+def login():
+    printf("login page")
+    return "index"
 if __name__ == "__main__":
     app.run(debug=True)
